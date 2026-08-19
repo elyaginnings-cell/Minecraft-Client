7f66a32a9b1295a35f29ae475d9c96eb31de5c1e
elyaginnings-cell <ely.a.ginnings@gmail.com>
2026-08-18 23:01:46 -0500

    Fix HudRenderCallback signature for 1.21.11 (DeltaTracker)

--- src/client/java/dev/gatto/client/hud/HudManager.java
+++ src/client/java/dev/gatto/client/hud/HudManager.java
@@ -3,6 +3,7 @@ package dev.gatto.client.hud;
 import dev.gatto.client.GattoClient;
 import dev.gatto.client.module.Module;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphics;
 
 public class HudManager {
@@ -10,7 +11,8 @@ public class HudManager {
         HudRenderCallback.EVENT.register(this::onHudRender);
     }
 
-    private void onHudRender(GuiGraphics graphics, float tickDelta) {
+    private void onHudRender(GuiGraphics graphics, DeltaTracker deltaTracker) {
+        float tickDelta = deltaTracker.getGameTimeDeltaPartialTick(false);
         for (Module module : GattoClient.getInstance().getModuleManager().getModules()) {
             if (module.isEnabled()) {
                 module.onHudRender(graphics, tickDelta);
